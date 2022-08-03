5bb315b1cebd58cb2e415f8fe524dce454c47d4a
frank <francortizm238@gmail.com>
2022-08-03 12:52:37 -0600

    first commit

---
+++ src/components/Toc.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>tomaisto</h1>
+    <button @click="alertaAueropuerto()" >wey</button>
+</template>
+
+<script  lang="ts">
+export default {
+  name: "Navbar",
+  components: {
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    alertaAueropuerto() {
+      alert('hodi')
+    },
+  }
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ src/components/Toc2.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default{
+setup() {
+    function odi2(message: string){
+        alert(message)
+    }
+    return{
+        odi2
+    }
+}
+}
+</script>
+<template>
+    <h2>segunfo componente</h2>
+    <button @click="odi2(123)">hi2</button>
+</template>
+
+
+<style lang="">
+    
+</style>
