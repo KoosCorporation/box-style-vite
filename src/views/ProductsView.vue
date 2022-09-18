9c2102fd8fa011c42999a200a6948d34fab8a07a
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-09-18 16:47:29 -0600

    Product view some bugs fixed
    weird product border fixed & Sass files organized

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,7 +1,10 @@
 <template >
     <div>
-        <h1>{{params.category}}</h1>
-        <h2>hodi</h2>
+        <div class="pruebitaa">
+            <h1>{{params.category}}</h1>
+        </div>
+            
+        <h2>a</h2>
         <CategoriesComponent :categoryRoute = "params.category"/>
     </div>
 </template>
@@ -10,8 +13,7 @@ import { useRoute } from "vue-router";
 import CategoriesComponent from "../components/CategoriesComponent.vue";
 const params =  useRoute().params
 </script>
+
 <style lang="scss" scoped>
-
-
-    
+  
 </style>
