943bc9615a6f22864f8bf2f88f8ca0b2cb70c748
frank <francortizm238@gmail.com>
2022-09-18 21:36:57 -0600

    img reactive finished

--- src/views/SingleProductView.vue
+++ src/views/SingleProductView.vue
@@ -9,32 +9,32 @@
         <div class="SingleProductView__container">
             <div class="SingleProductView__container-contImg">
                 <div class="SingleProductView__container-contImg-inside">
-                    <img src="../assets/img/products/product-1.png">
+                    <img :src="'/imgs/Test/img-'+imgPrincipal+'.jpg'">
                 </div>
             </div>
 
-            <div class="SingleProductView__container-contPreview">
-                <div class="SingleProductView__container-contPreview-contImg">
+            <div class="SingleProductView__container-contPreview" >
+                <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(1)">
                     <div class="SingleProductView__container-contPreview-img">
-                        <img src="../assets/img/products/product-1.png">
+                        <img src="/imgs/Test/img-1.jpg" >
                     </div>
                 </div>
 
-                <div class="SingleProductView__container-contPreview-contImg">
+                <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(2)">
                     <div class="SingleProductView__container-contPreview-img">
-                        <img src="../assets/img/products/product-1.png">
+                        <img src="/imgs/Test/img-2.jpg">
                     </div>
                 </div>
 
-                <div class="SingleProductView__container-contPreview-contImg">
+                <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(3)">
                     <div class="SingleProductView__container-contPreview-img">
-                        <img src="../assets/img/products/product-1.png">
+                        <img src="/imgs/Test/img-3.jpg">
                     </div>
                 </div>
 
-                <div class="SingleProductView__container-contPreview-contImg">
+                <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(4)">
                     <div class="SingleProductView__container-contPreview-img">
-                        <img src="../assets/img/products/product-1.png">
+                        <img src="/imgs/Test/img-4.jpg">
                     </div>
                 </div>
             </div>  
@@ -45,11 +45,18 @@
         </div>
 </template>
 <script lang="ts" setup>
+import {ref} from 'vue'
 import { useRoute } from 'vue-router';
 import { getProducts } from '../stores/products'
 
 const params = useRoute().params
 const singelProductGeted = getProducts().getProducById(Number(params.id))
+
+const imgPrincipal = ref(1)
+
+const changeImgPrincipal = (value: number) =>{
+    imgPrincipal.value = value
+}
 </script>
 <style lang="scss">
     @use '../SCSS/abstract' as *;
