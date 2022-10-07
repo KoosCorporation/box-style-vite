91f05430552c760a722808823a9c7b4ad4fff781
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-10-06 22:11:03 -0600

    Single product page done

--- src/views/SingleProductView.vue
+++ src/views/SingleProductView.vue
@@ -1,18 +1,18 @@
 <template>
 
-<!-- <h1>{{params.id}}</h1>
+    <!-- <h1>{{params.id}}</h1>
         <h1>{{singelProductGeted.title}}</h1>
         <h1>{{singelProductGeted.precio}}</h1>
         <h1>{{singelProductGeted.category}}</h1> -->
     <div class="SingleProductView">
-    
+
         <div class="SingleProductView__container">
             <div class="SingleProductView__container-contImg">
                 <div class="SingleProductView__container-contImg-inside">
                     <img :src="'/imgs/Test/img-'+imgPrincipal+'.jpg'">
                 </div>
             </div>
-    
+
             <div class="SingleProductView__container-contPreview">
                 <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(1)"
                     :style="'background-color: '+backgroundColor[0]+';'">
@@ -20,21 +20,21 @@
                         <img src="/imgs/Test/img-1.jpg">
                     </div>
                 </div>
-    
+
                 <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(2)"
                     :style="'background-color: '+backgroundColor[1]+';'">
                     <div class="SingleProductView__container-contPreview-img">
                         <img src="/imgs/Test/img-2.jpg">
                     </div>
                 </div>
-    
+
                 <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(3)"
                     :style="'background-color: '+backgroundColor[2]+';'">
                     <div class="SingleProductView__container-contPreview-img">
                         <img src="/imgs/Test/img-3.jpg">
                     </div>
                 </div>
-    
+
                 <div class="SingleProductView__container-contPreview-contImg" @click="changeImgPrincipal(4)"
                     :style="'background-color: '+backgroundColor[3]+';'">
                     <div class="SingleProductView__container-contPreview-img">
@@ -45,25 +45,41 @@
         </div>
 
         <div class="SingleProductView__cont-info">
-           <h1>Tittle</h1>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, illo.</p>
-           
-           <div class="SingleProductView__cont-info-Bottom">
-            <div class="SingleProductView_Size">
-                Talla: <span>M</span>
+            <h1>{{singelProductGeted.title}}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, illo.</p>
+
+            <div class="SingleProductView__cont-info-Bottom">
+                <div class="SingleProductView_Size">
+                    Talla: <span>M</span>
+                </div>
+
+                <h2>
+                    ${{singelProductGeted.precio}}
+                </h2>
             </div>
 
-            <h2>
-                $18.99
-            </h2>
-           </div>
+            <div class="SingleProductView_how">
+                <h2>Como comprar ? </h2>
+
+                <p>Si te gusto este producto tomale una captura y envialo a cualquiera de nuestras redes sociales.</p>
+                <div class="SingleProductView_how__RS">
+                    <!-- whatsapp // ------------------------ -->
+                    <a href="">
+                        <svg class="SingleProductView_how__whatsapp" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24">
+                            <path
+                                d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+
 
         </div>
 
 
-    
-    
-    
+
+
+
     </div>
 </template>
 <script lang="ts" setup>
